18a4d3469b3c5a644ec36588d7ac5dd8fe5bd421
CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
2026-01-16 16:24:06 -0800

    Separate physical/virtual petal containers
    commit b2351f50fb1c7c463f3d94126fc942a1685e10a2
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Thu Jan 15 22:42:26 2026 -0800
    
        Save physical/virtual flags for petals
    
    commit cb2644e4ebd08b235c2dea514f7aac404623bdae
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Thu Jan 15 19:02:45 2026 -0800
    
        Use flag for physical petal containers
    
    commit 2c6441a4dca988c56f07fee3a95f033faf8287f1
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Wed Jan 14 21:18:28 2026 -0800
    
        Use regex for flag matching
    
    commit d7ca1f347d9f97bed88e48f1dab37c79f68fde9f
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Wed Jan 14 20:57:13 2026 -0800
    
        Add support for BP containers
    
    commit 6a5c0afa455599fb6d5b454c6c3ff7d907b7d832
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Wed Jan 14 20:40:33 2026 -0800
    
        Fix menu patch to only include petal menu
    
    commit 73ce6191d5304f0e1fb6cafdf06f93f6fb5848a8
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Tue Jan 13 20:57:35 2026 -0800
    
        Handle hook when pause menu is up
    
    commit 83e28660dd014cbe635d5224e258ade89d3e5102
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Tue Jan 13 20:29:35 2026 -0800
    
        Hook into Game.GetFlagBool instead
    
    commit 52a13abe805214c57f6b7e8994e2fcf2a20f4c2c
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Tue Jan 13 18:17:14 2026 -0800
    
        Rename GlobalState variables
    
    commit 22963f2a39594f8a0197c2f9f8eeaea18f5d03bb
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Tue Jan 13 18:11:02 2026 -0800
    
        Filler
    
    commit 6ad0ac02d034016f8b2b9d04539f9c284113370e
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Tue Jan 13 17:24:39 2026 -0800
    
        Add logic for badge
    
    commit 6ebf339203d55075e38603f6cdeb7c567a5a40de
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Mon Jan 12 23:41:44 2026 -0800
    
        Replace dialogue text while getting petal
    
    commit 73142dbb8ca74c24a237e1fc069e35e9ca8b2cc6
    Author: CertifiedPyro <dwang733+CertifiedPyro@outlook.com>
    Date:   Mon Jan 12 22:10:41 2026 -0800
    
        Patch so physical/virtual petal container separate

--- Core.cs
+++ Core.cs
@@ -1,8 +1,6 @@
 ﻿using HarmonyLib;
 using Il2CppPipistrello;
 using MelonLoader;
-using System.Diagnostics;
-using System.Reflection;
 using UnityEngine;
 
 [assembly: MelonInfo(typeof(PipistrelloArchipelago.Core), "PipistrelloArchipelago", "0.1.0", "CertifiedPyro", null)]
@@ -10,122 +8,232 @@ using UnityEngine;
 
 namespace PipistrelloArchipelago;
 
-[HarmonyLib.HarmonyPatch(typeof(Director), nameof(Director.Init))]
+public static class GlobalState
+{
+    public static bool RunningInitRoom = false;
+    public static bool RunningCalculateIsHousePuzzleCompleted = false;
+    public static bool RunningPetalContainerMenu = false;
+    public static bool RunningBpContainerMenu = false;
+
+    public static UIDialog CurrentPetalContainerMenu;
+    public static UIDialog CurrentBpContainerMenu;
+
+    public static bool AcquiringPhysicalPetalContainer = false;  // True while obtaining physical petal container
+    public static bool AcquiringVirtualPetalContainer = false;  // True while Archipelago server gives us the petal container
+
+    public static bool AcquiringPhysicalBpContainer = false;  // True while obtaining physical BP container
+    public static bool AcquiringVirtualBpContainer = false;  // True while Archipelago server gives us the BP container
+    public static bool IsPhysicalBpContainerAcquired = false;  // True if physical BP container is gone
+
+    public static bool AcquiringPhysicalBadge = false;  // True while obtaining physical badge
+    public static bool AcquiringVirtualBadge = false;  // True while Archipelago server gives us the badge
+    public static bool IsPhysicalBadgeAcquired = false;  // True if physical badge is gone
+}
+
+[HarmonyPatch(typeof(Director))]
 public class DirectorPatch
 {
     public static Director instance;
 
+    [HarmonyPatch(nameof(Director.Init))]
     public static void Postfix(Director __instance)
     {
         instance = __instance;
-        MelonLogger.Msg("Director Reference Captured!");
+    }
+
+    [HarmonyPatch(nameof(Director.InitRoom))]
+    public static void Prefix()
+    {
+        GlobalState.RunningInitRoom = true;
+    }
+
+    [HarmonyPatch(nameof(Director.InitRoom))]
+    public static void Postfix()
+    {
+        GlobalState.RunningInitRoom = false;
     }
 }
 
-[HarmonyLib.HarmonyPatch(typeof(ObjectPetalContainer))]
-public class ObjectPetalContainerPatch
+[HarmonyPatch(typeof(Game), nameof(Game.GetFlagBool))]
+public class GameGetFlagBoolPatch
 {
-    public static ObjectPetalContainer instance;
+    public static void Postfix(string flag, ref bool __result)
+    {
+        // If original flag is true, then no need to check physical/virtual flags.
+        if (__result)
+        {
+            return;
+        }
 
-    //[HarmonyLib.HarmonyPatch(nameof(ObjectPetalContainer.Draw))]
-    //public static bool Prefix(ObjectPetalContainer __instance)
-    //{
-    //    instance = __instance;
-    //    return false;
-    //}
-
-    // Isn't called??
-    //[HarmonyLib.HarmonyPatch(nameof(ObjectPetalContainer.isCollectibleWithVisualEffects), HarmonyLib.MethodType.Getter)]
-    //public static bool Prefix(ObjectPetalContainer __instance, ref bool __result)
-    //{
-    //    instance = __instance;
-    //    __result = false;
-    //    return false;
-    //}
-
-    // Isn't called??
-    //[HarmonyLib.HarmonyPatch(nameof(ObjectPetalContainer.CheckForAcquisition))]
-    //public static bool Prefix(ObjectPetalContainer __instance, ref bool __result)
-    //{
-    //    instance = __instance;
-    //    __result = false;
-    //    MelonLogger.Msg($"Making petal container {__instance.petalContainerId} not acquirable");
-    //    return false;
-    //}
-
-    // Prevents pickup, but doesn't prevent it from being drawn if it exists, or not drawn if it doesn't exist
-    // Isn't called if it doesn't exist
-    //[HarmonyLib.HarmonyPatch(nameof(ObjectPetalContainer.Process))]
-    //public static bool Prefix(ObjectPetalContainer __instance)
-    //{
-    //    if (__instance.petalContainerId == "city/ren223/yug5534")
-    //    {
-    //        instance = __instance;
-    //        MelonLogger.Msg($"Calling petal container {__instance.petalContainerId} Process()");
-    //    }
-
-    //    return false;
-    //}
-
-    //[HarmonyLib.HarmonyPatch(nameof(ObjectPetalContainer.Register))]
-    //public static void Prefix()
-    //{
-    //    MelonLogger.Msg("Caling ObjectPetalContainer.Register");
-    //}
+        var basePhysicalPatch = GlobalState.RunningInitRoom || GlobalState.RunningCalculateIsHousePuzzleCompleted;
+        var petalPhysicalPatch = basePhysicalPatch || GlobalState.RunningPetalContainerMenu;
+        if (Utils.GetPetalIdFromFlag(flag, out var petalId) && petalId == "city/ren223/yug5534")
+        {
+            if (petalPhysicalPatch)
+            {
+                var physicalFlag = Utils.GetPetalPhysicalFlag(petalId);
+                __result = DirectorPatch.instance.GetFlagBool(physicalFlag);
+                MelonLogger.Msg($"Getting {physicalFlag}: {__result}");
+                return;
+            }
+            else
+            {
+                var virtualFlag = Utils.GetPetalVirtualFlag(petalId);
+                __result = DirectorPatch.instance.GetFlagBool(virtualFlag);
+                MelonLogger.Msg($"Getting {virtualFlag}: {__result}");
+                return;
+            }
+        }
+    }
 }
 
-// Program flow??
-//  - Director.InitRoom() - Once per room, even if visiting again
-//  - ??
-//  - Game.IsPetalContainerAcquired()
-//  - Director.CreateObject() - For each object in room, even if visiting again?
-//      - If petal container is acquired, CreateObject() is not called
-// Note: petal containers not contained in director.room.objects (Mapvania.Room), but is called
-// Note: petal containers not contained in director.currentProject.petalContainerMeta (Mapvania.Project), just contains all petal container coords, but is called
+[HarmonyPatch(typeof(Game), nameof(Game.SetFlagBool))]
+public class GameSetFlagBoolPatch
+{
+    public static bool Prefix(string flag, ref bool value)
+    {
+        if (Utils.GetPetalIdFromFlag(flag, out var petalId) && petalId == "city/ren223/yug5534")
+        {
+            var physicalFlag = Utils.GetPetalPhysicalFlag(petalId);
+            var virtualFlag = Utils.GetPetalVirtualFlag(petalId);
+            if (GlobalState.AcquiringPhysicalPetalContainer)
+            {
+                DirectorPatch.instance.SetFlagBool(physicalFlag, value);
+                MelonLogger.Msg($"Setting {physicalFlag}: {value}");
+            }
+            if (GlobalState.AcquiringVirtualPetalContainer)
+            {
+                DirectorPatch.instance.SetFlagBool(virtualFlag, value);
+                MelonLogger.Msg($"Setting {virtualFlag}: {value}");
+            }
 
-//[HarmonyLib.HarmonyPatch(typeof(Director))]
-//public class DirectorPatchDebug
-//{
-//    public static Director instance;
+            // Only set original acquired flag if physical and virtual flags are true.
+            value = DirectorPatch.instance.GetFlagBool(physicalFlag) && DirectorPatch.instance.GetFlagBool(virtualFlag);
+            MelonLogger.Msg($"Setting {flag}: {value}");
+            return true;
+        }
 
-//    [HarmonyLib.HarmonyPatch(nameof(Director.InitRoom))]
-//    public static void Prefix(Director __instance)
-//    {
-//        instance = __instance;
-//        if (instance.currentProject == null)
-//        {
-//            return;
-//        }
+        return true;
+    }
+}
 
-//        MelonLogger.Msg(instance.room.id);
-//        foreach (var petalContainerMeta in instance.currentProject.petalContainerMeta)
-//        {
-//            MelonLogger.Msg($"{petalContainerMeta.id} - {petalContainerMeta.room.id}");
-//        }
-//        MelonLogger.Msg("-------------------------------------------\n");
-//    }
-//}
+[HarmonyPatch(typeof(Game))]
+public class GamePatch
+{
+    // Runs as petal container is picked up (e.g. before text shows up)
+    [HarmonyPatch(nameof(Game.SetPetalContainerAcquired))]
+    [HarmonyPrefix]
+    public static bool PrefixSetPetalContainerAcquired(string id, bool acquired, ref bool __result)
+    {
+        // If we're acquiring the petal container from Archipelago server, allow the original method to run.
+        if (GlobalState.AcquiringVirtualPetalContainer)
+        {
+            return true;
+        }
 
-//[HarmonyLib.HarmonyPatch(typeof(Game))]
-//public class GamePatch
-//{
-//    [HarmonyLib.HarmonyPatch(nameof(Game.IsPetalContainerAcquired))]
-//    public static void Prefix(string id)
-//    {
-//        MelonLogger.Msg($"IsPetalContainerAcquired(): {id}");
+        // Otherwise, hijack the logic to not actually acquire the petal container.
+        GlobalState.AcquiringPhysicalPetalContainer = true;
+        DirectorPatch.instance.SetFlagBool(Game.FlagPetalContainerAcquired(id), acquired);
+        // Only set if acquired=true, since it's set to false when dialogue panel closes.
+        GlobalState.AcquiringPhysicalPetalContainer = acquired;
 
-//        StackTrace stackTrace = new StackTrace(true);
-//        StackFrame[] frames = stackTrace.GetFrames();
-//        for (int i = 1; i < frames.Length; i++)
-//        {
-//            MethodBase method = frames[i].GetMethod();
-//            if (method == null) continue;
+        __result = false;
+        return false;
+    }
+}
 
-//            // Log the class and method name
-//            MelonLogger.Msg($"{i}: {method.DeclaringType?.Name}.{method.Name}");
-//        }
-//    }
-//}
+[HarmonyPatch(typeof(DialoguePanel))]
+public class DialoguePanelPatch
+{
+    // DialoguePanel InjectText(): [instant|You got a [c:rose|Petal Container]!][w:3]
+    // DialoguePanel InjectText(): [c:rose|Petals] collected so far: [c:blue|1].[d]\n Collect 8 to increase your [c|maximum life points]!  
+    [HarmonyPatch(nameof(DialoguePanel.InjectText))]
+    public static void Prefix(ref string text)
+    {
+        if (GlobalState.AcquiringPhysicalPetalContainer)
+        {
+            //MelonLogger.Msg($"DialoguePanel InjectText() original args: {text}");
+            text = "[instant|You got a [c:blue|Archipelago item]!][w:2]";
+        }
+    }
+ 
+    // Remove replace text once dialogue is over.
+    [HarmonyPatch(nameof(DialoguePanel.IsOver))]
+    public static void Postfix(ref bool __result)
+    {
+        if (__result)
+        {
+            if (GlobalState.AcquiringPhysicalPetalContainer)
+            {
+                GlobalState.AcquiringPhysicalPetalContainer = false;
+            }
+        }
+    }
+}
+
+[HarmonyPatch(typeof(ObjectWarpArea))]
+public class ObjectWarpAreaPatch
+{
+    [HarmonyPatch(nameof(ObjectWarpArea.CalculateIsHousePuzzleCompleted))]
+    public static void Prefix()
+    {
+        GlobalState.RunningCalculateIsHousePuzzleCompleted = true;
+    }
+
+    [HarmonyPatch(nameof(ObjectWarpArea.CalculateIsHousePuzzleCompleted))]
+    public static void Postfix()
+    {
+        GlobalState.RunningCalculateIsHousePuzzleCompleted = false;
+    }
+}
+
+[HarmonyPatch(typeof(Menu))]
+public class MenuPatch
+{
+    [HarmonyPatch(nameof(Menu.MakePetalContainerMenu))]
+    [HarmonyPrefix]
+    public static void PrefixPetalContainerMenu()
+    {
+        GlobalState.RunningPetalContainerMenu = true;
+    }
+
+    [HarmonyPatch(nameof(Menu.MakePetalContainerMenu))]
+    public static void PostfixPetalContainerMenu(ref UIDialog __result)
+    {
+        GlobalState.CurrentPetalContainerMenu = __result;
+    }
+
+    [HarmonyPatch(nameof(Menu.MakeBpContainerMenu))]
+    public static void PrefixBpContainerMenu()
+    {
+        GlobalState.RunningBpContainerMenu = true;
+    }
+
+    [HarmonyPatch(nameof(Menu.MakeBpContainerMenu))]
+    public static void PostfixBpContainerMenu(ref UIDialog __result)
+    {
+        GlobalState.CurrentBpContainerMenu = __result;
+    }
+}
+
+[HarmonyPatch(typeof(UIDialog))]
+public class UIDialogPatch
+{
+    [HarmonyPatch(nameof(UIDialog.Close))]
+    public static void Postfix(UIDialog __instance)
+    {
+        if (__instance == GlobalState.CurrentPetalContainerMenu)
+        {
+            GlobalState.RunningPetalContainerMenu = false;
+            GlobalState.CurrentPetalContainerMenu = null;
+        }
+        else if (__instance == GlobalState.CurrentBpContainerMenu)
+        {
+            GlobalState.RunningBpContainerMenu = false;
+            GlobalState.CurrentBpContainerMenu = null;
+        }
+    }
+}
 
 public class Core : MelonMod
 {
@@ -142,69 +250,99 @@ public class Core : MelonMod
         // Health (petal container)
         if (Input.GetKeyDown(addKey) && Input.GetKey(KeyCode.LeftControl))
         {
-            LoggerInstance.Msg("Adding petal container!");
+            LoggerInstance.Msg("Adding virtual petal container!");
 
             var director = DirectorPatch.instance;
-            var petalContainerName = Game.GetPetalContainerName(0);
-            //Game.SetPetalContainerAcquired(director, "test123", 1, true);
+            GlobalState.AcquiringVirtualPetalContainer = true;
             Game.SetPetalContainerAcquired(director, "city/ren223/yug5534", 1, true);
+            GlobalState.AcquiringVirtualPetalContainer = false;
 
-            LoggerInstance.Msg("Finished adding petal container!");
+            LoggerInstance.Msg("Finished adding virtual petal container!");
         }
         else if (Input.GetKeyDown(removeKey) && Input.GetKey(KeyCode.LeftControl))
         {
             LoggerInstance.Msg("Removing petal container!");
 
             var director = DirectorPatch.instance;
-            var petalContainerName = Game.GetPetalContainerName(0);
-            //Game.SetPetalContainerAcquired(director, "test123", 1, false);
+            GlobalState.AcquiringVirtualPetalContainer = true;
+            Game.SetPetalContainerAcquired(director, "city/ren223/yug5534", 1, false);
+            GlobalState.AcquiringVirtualPetalContainer = false;
             Game.SetPetalContainerAcquired(director, "city/ren223/yug5534", 1, false);
 
             LoggerInstance.Msg("Finished removing petal container!");
         }
-        // BP (BP container)
+        //// BP (BP container)
+        //else if (Input.GetKeyDown(addKey) && Input.GetKey(KeyCode.LeftAlt))
+        //{
+        //    LoggerInstance.Msg("Adding virtual BP container!");
 
-        // Upgrades
-        else if (Input.GetKeyDown(addKey) && Input.GetKey(KeyCode.LeftShift))
-        {
-            LoggerInstance.Msg("Adding upgrade!");
+        //    var director = DirectorPatch.instance;
+        //    GlobalState.AcquiringVirtualBpContainer = true;
+        //    Game.SetBpContainerAcquired(director, "city/yug5154/yug5202", 1, true);
+        //    GlobalState.AcquiringVirtualBpContainer = false;
 
-            var director = DirectorPatch.instance;
-            var upgrade = Game.GetUpgradeById("yoyoBounceAttackUp");
-            Game.SetUpgradeAcquired(director, upgrade, true);
+        //    LoggerInstance.Msg("Finished adding virtual BP container!");
+        //}
+        //else if (Input.GetKeyDown(removeKey) && Input.GetKey(KeyCode.LeftAlt))
+        //{
+        //    LoggerInstance.Msg("Removing BP container!");
 
-            LoggerInstance.Msg("Finished adding upgrade!");
-        }
-        else if (Input.GetKeyDown(removeKey) && Input.GetKey(KeyCode.LeftShift))
-        {
-            LoggerInstance.Msg("Removing upgrade!");
+        //    var director = DirectorPatch.instance;
+        //    GlobalState.AcquiringVirtualBpContainer = true;
+        //    Game.SetBpContainerAcquired(director, "city/yug5154/yug5202", 1, false);
+        //    GlobalState.AcquiringVirtualBpContainer = false;
+        //    GlobalState.IsPhysicalBpContainerAcquired = false;
+        //    // TODO: Remove flag?
 
-            var director = DirectorPatch.instance;
-            var upgrade = Game.GetUpgradeById("yoyoBounceAttackUp");
-            Game.SetUpgradeAcquired(director, upgrade, false);
+        //    LoggerInstance.Msg("Finished removing BP container!");
+        //}
+        //// Upgrades
+        //else if (Input.GetKeyDown(addKey) && Input.GetKey(KeyCode.LeftShift))
+        //{
+        //    LoggerInstance.Msg("Adding upgrade!");
 
-            LoggerInstance.Msg("Finished removing upgrade!");
-        }
-        // Badges
-        else if (Input.GetKeyDown(addKey))
-        {
-            LoggerInstance.Msg("Adding badge!");
+        //    var director = DirectorPatch.instance;
+        //    var upgrade = Game.GetUpgradeById("yoyoBounceAttackUp");
+        //    Game.SetUpgradeAcquired(director, upgrade, true);
 
-            var director = DirectorPatch.instance;
-            var equip = Game.GetEquipById("walkTheDogFireTrail");
-            Game.SetEquipAcquired(director, equip, true, true);
+        //    LoggerInstance.Msg("Finished adding upgrade!");
+        //}
+        //else if (Input.GetKeyDown(removeKey) && Input.GetKey(KeyCode.LeftShift))
+        //{
+        //    LoggerInstance.Msg("Removing upgrade!");
 
-            LoggerInstance.Msg("Finished adding badge!");
-        }
-        else if (Input.GetKeyDown(removeKey))
-        {
-            LoggerInstance.Msg("Removing badge!");
+        //    var director = DirectorPatch.instance;
+        //    var upgrade = Game.GetUpgradeById("yoyoBounceAttackUp");
+        //    Game.SetUpgradeAcquired(director, upgrade, false);
 
-            var director = DirectorPatch.instance;
-            var equip = Game.GetEquipById("walkTheDogFireTrail");
-            Game.SetEquipAcquired(director, equip, false, true);
+        //    LoggerInstance.Msg("Finished removing upgrade!");
+        //}
+        //// Badges
+        //else if (Input.GetKeyDown(addKey))
+        //{
+        //    LoggerInstance.Msg("Adding virtual badge!");
 
-            LoggerInstance.Msg("Finished removing badge!");
-        }
+        //    var director = DirectorPatch.instance;
+        //    GlobalState.AcquiringVirtualBadge = true;
+        //    var equip = Game.GetEquipById("stringedYoyoNoPierce");
+        //    Game.SetEquipAcquired(director, equip, true, true);
+        //    GlobalState.AcquiringVirtualBadge = false;
+
+        //    LoggerInstance.Msg("Finished adding virtual badge!");
+        //}
+        //else if (Input.GetKeyDown(removeKey))
+        //{
+        //    LoggerInstance.Msg("Removing badge!");
+
+        //    var director = DirectorPatch.instance;
+        //    GlobalState.AcquiringVirtualBadge = true;
+        //    var equip = Game.GetEquipById("stringedYoyoNoPierce");
+        //    Game.SetEquipAcquired(director, equip, false, true);
+        //    GlobalState.AcquiringVirtualBadge = false;
+        //    GlobalState.IsPhysicalBadgeAcquired = false;
+        //    // TODO: Remove flag?
+
+        //    LoggerInstance.Msg("Finished removing badge!");
+        //}
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,31 @@
+﻿using Il2CppPipistrello;
+using System.Text.RegularExpressions;
+
+namespace PipistrelloArchipelago;
+
+static class Utils
+{
+    private static readonly Regex PetalFlagRegex = new($@"^{Regex.Escape(Game.FLAG_PETALCONTAINER_PREFIX)}(?<id>.*):acquired", RegexOptions.Compiled);
+
+    public static string GetPetalPhysicalFlag(string id)
+    {
+        return $"{Game.FLAG_PETALCONTAINER_PREFIX}{id}:physicalAcquired";
+    }
+
+    public static string GetPetalVirtualFlag(string id)
+    {
+        return $"{Game.FLAG_PETALCONTAINER_PREFIX}{id}:virtualAcquired";
+    }
+
+    public static bool GetPetalIdFromFlag(string flag, out string id)
+    {
+        if (PetalFlagRegex.Match(flag) is { Success: true } match)
+        {
+            id = match.Groups["id"].Value;
+            return true;
+        }
+
+        id = null;
+        return false;
+    }
+}
