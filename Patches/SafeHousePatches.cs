eaa8bf24b8cbdd17873cf49c3a84b8259f8987c8
CertifiedPyro <certifiedpyro1@gmail.com>
2026-08-17 18:06:02 -0700

    Add lever to safe house to prevent softlocks

---
+++ Patches/SafeHousePatches.cs
@@ -0,0 +1,123 @@
+﻿using HarmonyLib;
+using Il2CppPipistrello;
+using Il2CppUtil;
+using UnityEngine;
+
+namespace PipistrelloArchipelago.Patches;
+
+[HarmonyPatch]
+public static class SafeHousePatches
+{
+    private const string LeverObjectId = "archResetLever";
+    private const string SignObjectId = "archResetSign";
+    private const string ResetFlag = "t:archResetExit";
+    private const string SignTextKey = "safehouse_leverSign";
+    
+    private static Game.GlobalObjectId _originalSafeHouseExitId;
+
+    [HarmonyPatch(typeof(Director), nameof(Director.LoadProject))]
+    [HarmonyPostfix]
+    public static void LoadProjectPatch()
+    {
+        // Add lever to Safe House to reset to South Plaza.
+        var map = Global.Director.currentProject.maps.ToArray().FirstOrDefault(m => m.id == "safehouse")!;
+        var room = map.rooms.ToArray().FirstOrDefault(r => r.id == "mig38")!;
+        var objects = room.objects;
+        if (objects.ToArray().FirstOrDefault(o => o.globalObjectId.objectId == LeverObjectId) == null)
+        {
+            room.objects.Add(
+                new Mapvania.Object
+                {
+                    objectDefId = "lor20",
+                    objectDefName = "lever",
+                    globalObjectId = new Game.GlobalObjectId
+                    {
+                        mapId = "safehouse",
+                        roomId = "mig38",
+                        objectId = LeverObjectId
+                    },
+                    position = new Vector2(9 * 16, 8 * 16),
+                    width = 16,
+                    height = 16,
+                    properties = JsonValue.Parse($"{{\"controlsFlag\": \"{ResetFlag}\", \"mode\": \"toggle\"}}"),
+                    usesFlags = true
+                });
+        }
+
+        // Add sign explaining what the lever does.
+        if (objects.ToArray().FirstOrDefault(o => o.globalObjectId.objectId == SignObjectId) == null)
+        {
+            room.objects.Add(
+                new Mapvania.Object
+                {
+                    objectDefId = "mig589",
+                    objectDefName = "sign",
+                    globalObjectId = new Game.GlobalObjectId
+                    {
+                        mapId = "safehouse",
+                        roomId = "mig38",
+                        objectId = SignObjectId
+                    },
+                    position = new Vector2(8 * 16, 8 * 16),
+                    width = 16,
+                    height = 16,
+                    properties = JsonValue.Parse(
+                        $"{{\"code\": \"this.scenePlay(\\\"{SignTextKey}\\\")\", \"hideShadow\": true}}"),
+                    usesFlags = true
+                });
+        }
+    }
+
+    [HarmonyPatch(typeof(Director), nameof(Director.SetFlagBool))]
+    [HarmonyPostfix]
+    public static void SetFlagBoolPatch(string flag, bool value)
+    {
+        if (flag != ResetFlag)
+        {
+            return;
+        }
+
+        if (value)
+        {
+            _originalSafeHouseExitId = Global.Director.FindSafehouseExit().globalObjectId;
+            Global.Director.playerRecord.safehouseExitId = new Il2CppSystem.Nullable<Game.GlobalObjectId>(
+                new Game.GlobalObjectId
+                {
+                    mapId = "city",
+                    roomId = "ren223",
+                    objectId = "lor366"
+                });
+            Global.State.Messages.Enqueue(
+                "[instant|[c:green|Unstuck enabled]: Safe House exit set to South Plaza.][w:2]");
+        }
+        else
+        {
+            Global.Director.playerRecord.safehouseExitId =
+                new Il2CppSystem.Nullable<Game.GlobalObjectId>(_originalSafeHouseExitId);
+            Global.State.Messages.Enqueue(
+                "[instant|[c:red|Unstuck disabled]: Safe House exit set back to original.][w:2]");
+        }
+    }
+
+    [HarmonyPatch(typeof(Localization), nameof(Localization.GetEntries))]
+    [HarmonyPrefix]
+    public static bool LocalizationPatch(
+        string stringId,
+        ref Il2CppSystem.Collections.Generic.List<Localization.Entry> __result)
+    {
+        if (stringId != SignTextKey)
+        {
+            return true;
+        }
+
+        __result = new Il2CppSystem.Collections.Generic.List<Localization.Entry>();
+        __result.Add(
+            new Localization.Entry
+            {
+                speaker = "sign[0]",
+                contents = "If you are soft-locked, hit this lever to go back to South Plaza."
+            });
+
+        return false;
+    }
+}
