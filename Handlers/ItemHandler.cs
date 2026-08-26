69fb5a3ac3c1b58fd73259fec55da5701b0bedf5
CertifiedPyro <certifiedpyro1@gmail.com>
2026-08-26 00:51:52 -0700

    Don't show server messages if no messages allowed

--- Handlers/ItemHandler.cs
+++ Handlers/ItemHandler.cs
@@ -82,7 +82,9 @@ internal static class ItemHandler
                             && setting.HasFlag(ItemReceiveMessagesSetting.Trap))
                         || (item.Flags == ItemFlags.None
                             && setting.HasFlag(ItemReceiveMessagesSetting.Filler))
-                        || item.Player?.Slot == 0; // Items granted by server have no flags, so always show.
+                        // Items granted by server have no flags, so always show if any messages are allowed.
+                        || (item.Player?.Slot == 0
+                            && setting != ItemReceiveMessagesSetting.None);
                     var itemMessageAllowed = !isItemFromLocalLocation && itemMessageAllowedFromSetting;
 
                     var result = HandleItem(item, itemMessageAllowed);
